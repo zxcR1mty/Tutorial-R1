e1a08d7278bc040b3d44aa383018de1ef050cbcb
R1mty <ssadrin419@gmail.com>
2026-06-28 22:40:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// script.js
+
+document.querySelectorAll('.card').forEach(card => {
+
+```
+card.addEventListener('mousemove', e => {
+
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.background =
+    `radial-gradient(circle at ${x}px ${y}px,
+    rgba(255,255,255,.08),
+    rgba(255,255,255,.01) 60%)`;
+
+});
+
+card.addEventListener('mouseleave', () => {
+
+    card.style.background =
+    'rgba(255,255,255,.01)';
+
+});
+```
+
+});
 document.addEventListener("mousemove", e=>{
 
 const bg=document.querySelector(".background");
